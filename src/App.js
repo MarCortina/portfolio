c91aa76f6c68628a3cd3c6f0e9be481a884db908
Mar Cortina <72997924+MarCortina@users.noreply.github.com>
2021-05-13 08:11:54 -0300

    mapa mapa agregado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import mapa from "./assets/mapa-bkg-1.png";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Kingdom under construction</h1>
+      <img src={mapa} alt="" className="" />
     </div>
   );
 }
