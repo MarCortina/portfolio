b63e6cc14cc970768f8f3635dedb5558341398f7
Mar Cortina <72997924+MarCortina@users.noreply.github.com>
2021-05-13 16:25:03 -0300

    tamaño mapa y letras ajustes del mapa y las letras del h1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import mapa from "./assets/mapa-bkg-1.png";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Kingdom under construction</h1>
-      <img src={mapa} alt="" className="" />
+    <div className="container">
+      <div className="App">
+        <header>
+          <h1>Kingdom under construction</h1>
+        </header>
+        <img src={mapa} alt="mapa" className="mapa-bkg" />
+      </div>
+      {console.log(mapa, "MAPA")}
     </div>
   );
 }
